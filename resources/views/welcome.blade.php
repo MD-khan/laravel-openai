6768f5047ccf14db0fc1c1cf62e2d65caa89947e
Md Khan <md.monir.khan707@gmail.com>
2024-03-08 09:30:20 -0500

    intrigrated openAI

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AI Poems</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="h-full grid place-items-center p-6">
+    <div class="text-xs font-sans">
+        {!! nl2br($poem) !!}
+    </div>
+</body>
+</html>
